a2700b5062737f7d8c66b913d2258fdc19213508
Sreedevi Aryasomayajula <115368231+017Asd@users.noreply.github.com>
2023-10-07 19:07:17 +0530

    Add files via upload

---
+++ 01_name.py
@@ -0,0 +1,13 @@
+a =(input("enter your number"))
+b =int(input("enter your number"))
+c =int(input("enter your number"))
+d =int(input("enter your number"))
+e =int(input("enter your number"))
+1 = 45
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+print(1)
